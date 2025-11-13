033d63a993ebc1fbf665e767358d3e8f850f8fc2
NiklasKlare <niklas.klare@outlook.com>
2025-11-13 09:55:38 +0100

    webApp 13.1. v2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b611037dd278086183af0699f3780358
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "b5434a8ec39e596b95a7502acb4bc90b",
+"flutter_bootstrap.js": "f9f0196f2ca368f72bba6a49edec6825",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
